68c956520eb55072b34b2d0902b5ab16db38e819
DanielKruts <djkrutsick@icloud.com>
2025-10-31 11:54:28 -0400

    Updated Code, Idk, I'm depressed

---
+++ GeneologyProject.pl
@@ -0,0 +1,22 @@
+factorial(1,1).
+factorial(X,Y):-
+	x>1,
+	A is X-1
+	factorial(A,B),
+	Y is B*X
+
+contains([H|_],H).
+contains([_|T],Y):-
+	contains(T,Y).
+
+containsall(_,[]).
+containsall([H|T],[H|T]):-
+	contains(X,H),
+	\+ contains(T,H),
+	containsall(X,T)
+
+perm(X,Y):-
+	length(X,Z),
+	length(Y,Z),
+	containsall(X,Y),
+        containsall(Y,X).
