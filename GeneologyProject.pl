3f5a6b5d62669aa1907da9926cfa0cc0cb989cfb
DanielKruts <djkrutsick@icloud.com>
2025-11-02 16:37:40 -0500

    More changes on Fact base before merge

--- GeneologyProject.pl
+++ GeneologyProject.pl
@@ -1,6 +1,34 @@
 %Facts (AKA, the large number of relations between our family tree
+%Gervin the Bold and Nivreg's children
 parent(Gervin_the_Bold, Gervin_the_Weird).
+parent(Gervin_the_Bold, Eden).
+parent(Nivreg, Gervin_the_Weird).
+parent(Nivreg, Eden).
 
+%Gervin the Weird and Rag's Children
+parent(Gervin_the_Weird, Gervin_the_Inferno).
+parent(Gervin_the_Weird, Quasimoto).
+parent(Rag, Gervin_the_Inferno).
+parent(Rag, Quasimoto).
+
+%Quasimoto and Frog's child
+parent(Quasimoto, Thea).
+parent(Frog, Thea).
+
+%Gervin the Inferno and Priscilla's children
+parent(Gervin_the_Inferno, Gervin_the_Reborn).
+parent(Gervin_the_Inferno, Vulkan).
+parent(Gervin_the_Inferno, Dorn).
+parent(Gervin_the_Inferno, Lion).
+parent(Priscilla, Gervin_the_Reborn).
+parent(Priscilla, Vulkan).
+parent(Priscilla, Dorn).
+parent(Priscilla, Lion).
+
+%Eden and Adam's children
+parent(Eden, Geoffry).
+parent(Eden, Sheoldred).
+parent(Eden, Titus).
 
 
 fib(1,1).
